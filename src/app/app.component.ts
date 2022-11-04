0e2c98bb1506222e18093952343f9e7f90f97428
alibek <topsnek495@gmail.com>
2022-11-04 22:39:46 +0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SplatoonUiPlugin';
+  wins = 0
+  losses = 0
+  lost = false
+  message = "BRUH!"
+  nums = [1,2,3,4,5]
+
+  @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+      if(event.key=="ArrowRight"){
+        this.LossesPlus()
+      } else if (event.key=="ArrowDown"){
+        this.WinsMinus()
+      } else if (event.key=="ArrowLeft"){
+        this.LossesMinus()
+      } else if (event.key=="ArrowUp"){
+        this.WinsPlus()
+      }
+      else if (event.key=="`"){
+        this.Reset()
+      }
+    }
+
+    WinsPlus(){
+      if(this.wins!=5){
+        this.wins++
+      }
+    }
+
+    WinsMinus(){
+      if(this.wins!=0){
+        this.wins--
+      }
+    }
+
+    LossesPlus(){
+      if(this.losses!=3){
+        this.losses++
+        // if(this.losses==3){
+        //   this.lost=true
+        //   setTimeout(() => 
+        //     {
+        //         this.Reset()
+        //     },
+        //     3000);
+        // } 
+      } 
+    }
+
+    LossesMinus(){
+      if(this.losses!=0){
+        this.losses--
+      }
+    }
+
+    Reset(){
+      this.wins=0
+      this.losses=0
+      this.lost=false
+    }
 }
